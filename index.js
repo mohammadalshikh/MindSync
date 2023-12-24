51d57d5423a6df43c1f46215fe2853c01369ab5f
Mohammad Alshikh <95369034+mohammadalshikh@users.noreply.github.com>
2023-12-24 15:03:03 -0500

    Added  nodemon locally

--- index.js
+++ index.js
@@ -1,44 +1,35 @@
 require('dotenv/config');
-const { Client } = require('discord.js');
-const { OpenAI } = require('openai');
+const discord = require('discord.js');
 
-const client = new Client({
-    intents: ['Guilds', 'GuildMembers', 'GuildMessages', 'MessageContent']
+const client = new discord.Client({
+    intents: [
+        discord.GatewayIntentBits.Guilds,
+        discord.GatewayIntentBits.GuildMessages,
+        discord.GatewayIntentBits.GuildMembers,
+        discord.GatewayIntentBits.MessageContent,
+        discord.GatewayIntentBits.DirectMessages
+    ],
+    partials: [
+        discord.Partials.Message,
+        discord.Partials.Channel,
+        discord.Partials.GuildMember,
+        discord.Partials.User,
+        discord.Partials.GuildScheduledEvent
+    ]
 });
 
 client.on('ready', () => {
-    console.log('MindSync is online.')
+    console.log(`${client.user.username} is online.`)
 });
 
-const CHANNELS = ['1182238025955164221'];
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_KEY,
-})
+const CHANNELS = ['1188317266757029898'];
 
 client.on('messageCreate', async (message) => {
     if (message.author.bot) return;
     if (message.content.startsWith("!")) return;
     if (!CHANNELS.includes(message.channelId) && !message.mentions.users.has(client.user.id)) return;
 
-    const response = await openai.chat.completions
-        .create({
-            model: 'gpt-3.5-turbo',
-            messages: [
-                {
-                    // name: 
-                    role: 'system',
-                    content: 'You are a chatbot named MindSync whose goal is to mimic my personality and style of texting.',
-                },
-                {
-                    // name:
-                    role: 'user',
-                    content: message.content,
-                }
-            ]
-        }).catch((error) => console.error('OpenAI error:\n', error));
-
-    message.reply(response.choices[0].message.content);
+    message.reply("You said: " + message.content);
 });
 
 client.login(process.env.TOKEN)
