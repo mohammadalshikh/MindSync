80a317e7b7c40d24be9895d6b9e0de3013156ab5
Mohammad Alshikh <95369034+mohammadalshikh@users.noreply.github.com>
2024-02-15 22:04:24 -0500

    Handling messageDelete edge cases

--- Events/Client/messageDelete.js
+++ Events/Client/messageDelete.js
@@ -5,14 +5,16 @@ module.exports = {
     name: 'messageDelete',
     once: false,
     execute(deletedMessage) {
-        const userData = loadUserData()
-        const userId = deletedMessage.components[0].components[0].data.custom_id.split('-')[2]
-        const name = deletedMessage.components[0].components[0].data.custom_id.split('-')[3]
-        const taskIndex = getTaskIdx(userId, name)
-        if (taskIndex != -1) {
-            userData[userId].tasks[taskIndex].deletedMsg = true
-            userData[userId].del.push(name)
+        if (deletedMessage.components[0] && deletedMessage.components[0].components[0] && deletedMessage.components[0].components[0].data && deletedMessage.components[0].components[0].data.custom_id) {
+            const userId = deletedMessage.components[0].components[0].data.custom_id.split('-')[2]
+            const name = deletedMessage.components[0].components[0].data.custom_id.split('-')[3]
+            const taskIndex = getTaskIdx(userId, name)
+            if (taskIndex != -1) {
+                const userData = loadUserData()
+                userData[userId].tasks[taskIndex].deletedMsg = true
+                userData[userId].del.push(name)
+                saveUserData(userData)
+            }
         }
-        saveUserData(userData)
     },
 }
